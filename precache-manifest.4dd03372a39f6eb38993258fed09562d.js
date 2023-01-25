b559ea7c7c712fa5581e9aea3d7bd51ada4f779b
Alfian Dwi Syahputra <alfiaansaputra@yahoo.com>
2023-01-25 15:08:42 +0700

    Updates

---
+++ precache-manifest.4dd03372a39f6eb38993258fed09562d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0bfa6471ce94da4d5251d7d466f3be3",
+    "url": "/test2/index.html"
+  },
+  {
+    "revision": "4a8a11f0109a3bedf961",
+    "url": "/test2/static/css/main.7f146f95.chunk.css"
+  },
+  {
+    "revision": "01c6d046ac983e9ea0b4",
+    "url": "/test2/static/js/2.7b5642a7.chunk.js"
+  },
+  {
+    "revision": "4a8a11f0109a3bedf961",
+    "url": "/test2/static/js/main.2c7575fe.chunk.js"
+  },
+  {
+    "revision": "872033a9efb6eb9fe611",
+    "url": "/test2/static/js/runtime~main.bb481ce1.js"
+  },
+  {
+    "revision": "87b5f04a82a6dc765fcfebba13b6d9c8",
+    "url": "/test2/static/media/aneka-makanan.87b5f04a.png"
+  }
+]);
